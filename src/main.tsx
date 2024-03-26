edb937becc2926a607941d7b1ed7b2093c2f60f1
Captain_Turtle <133596616+heren1heren@users.noreply.github.com>
2024-03-26 14:23:09 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import { Person } from './App';
 const root: HTMLElement = document.getElementById('root')!;
 ReactDOM.createRoot(root).render(
