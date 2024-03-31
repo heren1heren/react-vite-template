27678406d6eb64c8f2395ac36b4d27735c4219ba
heren <heren1heren@gmail.com>
2024-03-31 13:20:45 -0600

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export function Person() {
-  const [lastName, setLastName] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [person, setPerson] = useState({
-    name: firstName + ' ' + lastName,
-    age: 100,
-  });
-  const handleFirstName = (e) => {
-    setFirstName(e.target.value);
-    // setPerson({ ...person, name: `${firstName} ${lastName}` });
-  };
-  const handleLastName = (e) => {
-    setLastName(e.target.value);
-    // setPerson({ ...person, name: `${firstName} ${lastName}` });
-  };
-  const handleIncreaseAge = () => {
-    setPerson({ ...person, age: person.age + 1 });
-  };
+export default function Clock() {
+  const [counter, setCounter] = useState(0);
 
-  return (
-    <>
-      <h1>{firstName + ' ' + lastName}</h1>
-      <h2>{person.age}</h2>
-      <div>
-        <p>FirstName:</p>
-        <input onChange={handleFirstName} value={firstName} />
-      </div>
-      <div>
-        <p>LastName:</p>
-        <input onChange={handleLastName} value={lastName} />
-      </div>
-      <button onClick={handleIncreaseAge}>Increase age</button>
-    </>
-  );
+  setInterval(() => {
+    setCounter((count) => count + 1);
+  }, 1000);
+
+  return <p>{counter} seconds have passed.</p>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Person } from './App';
+import './index.scss';
+import Clock from './App';
 const root: HTMLElement = document.getElementById('root')!;
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <Person />
+    <Clock />
   </React.StrictMode>
 );
